727d217fadc6af1aa699066ad1b09c15a45d81b1
LYKAN <LYKAN@192.168.1.77>
2025-03-02 16:32:48 -0500

    carga inicial

---
+++ src/calculararea/principal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculararea;
+
+/**
+ *
+ * @author LYKAN
+ */
+public class principal {
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
